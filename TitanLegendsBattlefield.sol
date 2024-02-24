46f2fb078bf8a0b119a338d3b759015c6a94e326
TitanLegends <161147330+TitanLegends@users.noreply.github.com>
2024-02-24 10:58:18 -0500

    Add files via upload

---
+++ TitanLegendsBattlefield.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "erc721a/contracts/IERC721A.sol";
+import "./ILGNDX.sol";
+
+contract TitanLegendsBattlefield is ERC721Holder, Ownable {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    IERC721A public immutable titanLegends;
+    ILGNDX public immutable legendX;
+
+    uint256 public immutable tokensPerBountyPoint = 59_644.655 ether;
+    uint64 public maxPerBounty = 8;
+    uint64 public bountyCooldown = 8 days;
+    mapping(address => EnumerableSet.UintSet) private _battles;
+    mapping(address => uint256) public battleEntered;
+
+    event BountyClaimed(address addr, uint256[] tokenIds);
+    event RansomPaid(address addr, uint256[] tokenIds);
+
+    constructor(address _titanLegends, address _legendX) Ownable(msg.sender) {
+        titanLegends = IERC721A(_titanLegends);
+        legendX = ILGNDX(_legendX);
+    }
+
+    function dragonsAtBattle(
+        address account
+    ) external view returns (uint256[] memory) {
+        return _battles[account].values();
+    }
+
+    function bountyAvailable(address account) public view returns (bool) {
+        return block.timestamp - battleEntered[account] > bountyCooldown;
+    }
+
+    function getMultiplier(uint256 tokenId) public pure returns (uint256) {
+        if (
+            tokenId == 46 ||
+            tokenId == 135 ||
+            tokenId == 212 ||
+            tokenId == 225 ||
+            tokenId == 427 ||
+            tokenId == 532 ||
+            tokenId == 591 ||
+            tokenId == 694 ||
+            tokenId == 735 ||
+            tokenId == 811 ||
+            tokenId == 946 ||
+            tokenId == 1139 ||
+            tokenId == 1210 ||
+            tokenId == 1237 ||
+            tokenId == 1357 ||
+            tokenId == 1457 ||
+            tokenId == 1503 ||
+            tokenId == 1561 ||
+            tokenId == 1663 ||
+            tokenId == 1876 ||
+            tokenId == 1996 ||
+            tokenId == 2089 ||
+            tokenId == 2251
+        ) return 80;
+        if (tokenId > 1500) return 28;
+        return 10;
+    }
+
+    function claimBounty(uint256[] calldata tokenIds) external {
+        require(bountyAvailable(msg.sender), "You cannot claim bounty now");
+        require(
+            tokenIds.length <= maxPerBounty,
+            "Max number of dragons per bounty exceeded"
+        );
+        uint256 multiplierPool;
+        for (uint256 i; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            unchecked {
+                multiplierPool += getMultiplier(tokenId);
+            }
+            titanLegends.safeTransferFrom(msg.sender, address(this), tokenId);
+            _battles[msg.sender].add(tokenId);
+        }
+        uint256 bountyPool = multiplierPool * tokensPerBountyPoint;
+        uint256 burnPool = (bountyPool * 3) / 100;
+        legendX.burn(burnPool);
+        legendX.transfer(msg.sender, bountyPool - burnPool);
+        battleEntered[msg.sender] = block.timestamp;
+        emit BountyClaimed(msg.sender, tokenIds);
+    }
+
+    function payRansom(uint256[] calldata tokenIds) external {
+        uint256 multiplierPool;
+        for (uint256 i; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            require(
+                _battles[msg.sender].contains(tokenId),
+                "You are not the original owner"
+            );
+            unchecked {
+                multiplierPool += getMultiplier(tokenId);
+            }
+            titanLegends.safeTransferFrom(address(this), msg.sender, tokenId);
+            _battles[msg.sender].remove(tokenId);
+        }
+        uint256 ransomPool = multiplierPool * tokensPerBountyPoint;
+        uint256 burnPool = (ransomPool * 3) / 100;
+        legendX.transferFrom(msg.sender, address(this), ransomPool + burnPool);
+        legendX.burn(burnPool);
+        emit RansomPaid(msg.sender, tokenIds);
+    }
+
+    function setMaxPerBounty(uint64 _limit) external onlyOwner {
+        maxPerBounty = _limit;
+    }
+
+    function setBountyCooldown(uint64 _limit) external onlyOwner {
+        bountyCooldown = _limit;
+    }
+}
---
+++ TitanLegendsMarketplace.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "erc721a/contracts/IERC721A.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TitanLegendsMarketplace is ERC721Holder, ReentrancyGuard, Ownable {
+    struct Listing {
+        uint256 tokenId;
+        uint256 price;
+        address owner;
+    }
+
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    uint256 public currentListingId;
+
+    uint64 public marketplaceFee;
+
+    address private feeStorage;
+    IERC721A public titanLegends;
+    IERC20 public titanX;
+
+    mapping(uint256 => Listing) public listings;
+    EnumerableSet.UintSet private activeListings;
+
+    event ListingAdded(
+        uint256 indexed listingId,
+        uint256 indexed tokenId,
+        address indexed owner,
+        uint256 price
+    );
+
+    event ListingRemoved(uint256 indexed listingId);
+    event ListingEdited(uint256 indexed listingId, uint256 price);
+    event ListingSold(uint256 indexed listingId, address buyer);
+
+    constructor(
+        address nftAddress,
+        address tokenAddress,
+        address feeStorageAddress
+    ) Ownable(msg.sender) {
+        titanLegends = IERC721A(nftAddress);
+        titanX = IERC20(tokenAddress);
+        marketplaceFee = 300;
+        feeStorage = feeStorageAddress;
+    }
+
+    function addListing(uint256 tokenId, uint256 price) public nonReentrant {
+        require(tx.origin == _msgSender(), "Contracts are prohibited");
+        require(price > 0, "Price must be greater than zero");
+        require(!isListingActive(currentListingId), "Listing already active");
+        uint256 listingId = currentListingId;
+        titanLegends.safeTransferFrom(msg.sender, address(this), tokenId);
+        listings[listingId] = Listing(tokenId, price, msg.sender);
+
+        activeListings.add(listingId);
+        currentListingId++;
+        emit ListingAdded(listingId, tokenId, msg.sender, price);
+    }
+
+    function buyListing(uint256 listingId) public payable nonReentrant {
+        require(tx.origin == _msgSender(), "Contracts are prohibited");
+        require(isListingActive(listingId), "Listing is not active");
+        Listing memory listing = listings[listingId];
+        uint256 _marketplaceFee = (listing.price * marketplaceFee) / 10000;
+        titanX.transferFrom(msg.sender, feeStorage, _marketplaceFee);
+        titanX.transferFrom(
+            msg.sender,
+            listing.owner,
+            listing.price - _marketplaceFee
+        );
+        titanLegends.safeTransferFrom(
+            address(this),
+            msg.sender,
+            listing.tokenId
+        );
+
+        activeListings.remove(listingId);
+        delete listings[listingId];
+
+        emit ListingSold(listingId, msg.sender);
+    }
+
+    function removeListing(uint256 listingId) public nonReentrant {
+        require(tx.origin == _msgSender(), "Contracts are prohibited");
+        require(isListingActive(listingId), "Listing is not active");
+        Listing memory listing = listings[listingId];
+        require(listing.owner == _msgSender(), "Not authorized");
+        titanLegends.safeTransferFrom(
+            address(this),
+            msg.sender,
+            listing.tokenId
+        );
+
+        activeListings.remove(listingId);
+        delete listings[listingId];
+        emit ListingRemoved(listingId);
+    }
+
+    function editListing(uint256 listingId, uint256 newPrice) public {
+        require(tx.origin == _msgSender(), "Contracts are prohibited");
+        require(isListingActive(listingId), "Listing is not active");
+        Listing storage listing = listings[listingId];
+        require(listing.owner == _msgSender(), "Not authorized");
+        require(newPrice > 0, "Price need to be higher than 0");
+
+        listing.price = newPrice;
+        emit ListingEdited(listingId, newPrice);
+    }
+
+    function getActiveListings() external view returns (uint256[] memory) {
+        return activeListings.values();
+    }
+
+    function isListingActive(uint256 listingId) public view returns (bool) {
+        return activeListings.contains(listingId);
+    }
+
+    function setMarketplaceFee(uint64 fee) external onlyOwner {
+        require(fee <= 800, "MArketplace fee should not exceed 8 percent");
+        marketplaceFee = fee;
+    }
+}
