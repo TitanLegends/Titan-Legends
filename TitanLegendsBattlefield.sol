00938553dee5df0a6ddcf003222aa5b5b32a5d52
Titan Legends / $LGNDX <161147330+TitanLegends@users.noreply.github.com>
2024-03-13 15:12:36 -0400

    Update TitanLegendsBattlefield.sol

--- TitanLegendsBattlefield.sol
+++ TitanLegendsBattlefield.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+import "@openzeppelin/contracts/interfaces/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import "erc721a/contracts/IERC721A.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./ILGNDX.sol";
 
-contract TitanLegendsBattlefield is ERC721Holder, Ownable {
+contract TitanLegendsBattlefield is ERC721Holder, ReentrancyGuard, Ownable2Step {
     using EnumerableSet for EnumerableSet.UintSet;
 
-    IERC721A public immutable titanLegends;
+    IERC721 public immutable titanLegends;
     ILGNDX public immutable legendX;
 
-    uint256 public immutable tokensPerBountyPoint = 59_644.655 ether;
+    uint256 public constant tokensPerBountyPoint = 59_644.655 ether;
     uint64 public maxPerBounty = 8;
     uint64 public bountyCooldown = 8 days;
     mapping(address => EnumerableSet.UintSet) private _battles;
@@ -23,13 +24,11 @@ contract TitanLegendsBattlefield is ERC721Holder, Ownable {
     event RansomPaid(address addr, uint256[] tokenIds);
 
     constructor(address _titanLegends, address _legendX) Ownable(msg.sender) {
-        titanLegends = IERC721A(_titanLegends);
+        titanLegends = IERC721(_titanLegends);
         legendX = ILGNDX(_legendX);
     }
 
-    function dragonsAtBattle(
-        address account
-    ) external view returns (uint256[] memory) {
+    function dragonsAtBattle(address account) external view returns (uint256[] memory) {
         return _battles[account].values();
     }
 
@@ -39,40 +38,19 @@ contract TitanLegendsBattlefield is ERC721Holder, Ownable {
 
     function getMultiplier(uint256 tokenId) public pure returns (uint256) {
         if (
-            tokenId == 46 ||
-            tokenId == 135 ||
-            tokenId == 212 ||
-            tokenId == 225 ||
-            tokenId == 427 ||
-            tokenId == 532 ||
-            tokenId == 591 ||
-            tokenId == 694 ||
-            tokenId == 735 ||
-            tokenId == 811 ||
-            tokenId == 946 ||
-            tokenId == 1139 ||
-            tokenId == 1210 ||
-            tokenId == 1237 ||
-            tokenId == 1357 ||
-            tokenId == 1457 ||
-            tokenId == 1503 ||
-            tokenId == 1561 ||
-            tokenId == 1663 ||
-            tokenId == 1876 ||
-            tokenId == 1996 ||
-            tokenId == 2089 ||
-            tokenId == 2251
+            tokenId == 46 || tokenId == 135 || tokenId == 212 || tokenId == 225 || tokenId == 427 || tokenId == 532
+                || tokenId == 591 || tokenId == 694 || tokenId == 735 || tokenId == 811 || tokenId == 946 || tokenId == 1139
+                || tokenId == 1210 || tokenId == 1237 || tokenId == 1357 || tokenId == 1457 || tokenId == 1503
+                || tokenId == 1561 || tokenId == 1663 || tokenId == 1876 || tokenId == 1996 || tokenId == 2089
+                || tokenId == 2251
         ) return 80;
         if (tokenId > 1500) return 28;
         return 10;
     }
 
-    function claimBounty(uint256[] calldata tokenIds) external {
+    function claimBounty(uint256[] calldata tokenIds) external nonReentrant {
         require(bountyAvailable(msg.sender), "You cannot claim bounty now");
-        require(
-            tokenIds.length <= maxPerBounty,
-            "Max number of dragons per bounty exceeded"
-        );
+        require(tokenIds.length <= maxPerBounty, "Max number of dragons per bounty exceeded");
         uint256 multiplierPool;
         for (uint256 i; i < tokenIds.length; i++) {
             uint256 tokenId = tokenIds[i];
@@ -90,14 +68,11 @@ contract TitanLegendsBattlefield is ERC721Holder, Ownable {
         emit BountyClaimed(msg.sender, tokenIds);
     }
 
-    function payRansom(uint256[] calldata tokenIds) external {
+    function payRansom(uint256[] calldata tokenIds) external nonReentrant {
         uint256 multiplierPool;
         for (uint256 i; i < tokenIds.length; i++) {
             uint256 tokenId = tokenIds[i];
-            require(
-                _battles[msg.sender].contains(tokenId),
-                "You are not the original owner"
-            );
+            require(_battles[msg.sender].contains(tokenId), "You are not the original owner");
             unchecked {
                 multiplierPool += getMultiplier(tokenId);
             }
@@ -119,3 +94,4 @@ contract TitanLegendsBattlefield is ERC721Holder, Ownable {
         bountyCooldown = _limit;
     }
 }
+
